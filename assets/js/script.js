912c65887f2419a2c1b091ed660abb0c44923f3a
Neha <neha@Nehas-MacBook-Air.local>
2025-06-03 14:22:10 +0530

    making responsive nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,10 @@ function toggleAccordion(selector) {
 
     clickedItem.classList.toggle('active');
 }
-  
+
+
+function menuIcon() {
+    document.querySelector('.menu-list').classList.toggle('show');
+    document.querySelector('nav').classList.toggle('activeNavBar');
+
+};
